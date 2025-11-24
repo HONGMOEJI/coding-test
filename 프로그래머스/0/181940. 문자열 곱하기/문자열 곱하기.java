420d31da83701e4f986e8618e4981352baae560f
Youngjun Hong <148158147+HONGMOEJI@users.noreply.github.com>
2025-11-25 00:00:57 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.11 ms, Memory: 84.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public String solution(String my_string, int k) {
+        StringBuilder builder = new StringBuilder();
+        
+        for(int i=0; i<k; i++) {
+            builder.append(my_string);
+        }
+        return builder.toString();
+    }
+}
