9702d85c4423180606cdbfd082e335e2cd9fae1f
Youngjun Hong <148158147+HONGMOEJI@users.noreply.github.com>
2025-11-25 22:38:22 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.04 ms, Memory: 87.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer = 0;
+        if(common[2]-common[1] == common[1]-common[0]) {
+            // 등차수열
+            int diff = common[1]-common[0];
+            answer = common[common.length-1] + diff;
+        } else {
+            // 등비수열
+            int ratio = common[1]/common[0];
+            answer = common[common.length-1] * ratio;
+        }
+        return answer;
+    }
+
+    
+}
