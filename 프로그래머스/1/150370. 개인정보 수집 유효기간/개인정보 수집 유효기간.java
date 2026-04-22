cb3b9d9d5e0b00cc66022d5375ed26896a1ffaff
Youngjun Hong <148158147+HONGMOEJI@users.noreply.github.com>
2026-04-23 03:12:20 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.99 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -1,39 +1,78 @@
-import java.util.*;
+import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        List<Integer> answer = new ArrayList<>();
-        Map<String, Integer> termMap = new HashMap<>();
+        int[] answer = {};
+        List<Integer> answerList = new ArrayList<>();
 
-        for (String term : terms) {
-            String[] parts = term.split(" ");
-            termMap.put(parts[0], Integer.parseInt(parts[1]));
-        }
-
-        int todayDays = toDays(today);
-
-        for (int i = 0; i < privacies.length; i++) {
-            String[] parts = privacies[i].split(" ");
-            String date = parts[0];
-            String type = parts[1];
-
-            int collectedDays = toDays(date);
-            int expireDays = collectedDays + termMap.get(type) * 28;
-
-            if (expireDays <= todayDays) {
-                answer.add(i + 1);
-            }
-        }
-
-        return answer.stream().mapToInt(Integer::intValue).toArray();
-    }
-
-    private int toDays(String date) {
-        String[] ymd = date.split("\\.");
+        String[] ymd = today.split("\\.");
         int year = Integer.parseInt(ymd[0]);
         int month = Integer.parseInt(ymd[1]);
         int day = Integer.parseInt(ymd[2]);
 
-        return year * 12 * 28 + month * 28 + day;
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String term : terms) {
+            String[] keyValue = term.split(" ");
+            map.put(keyValue[0], Integer.parseInt(keyValue[1]));
+        }
+
+        int index = 1;
+        for(String privacy : privacies) {
+            String type = privacy.split(" ")[1];
+            String aymd = privacy.split(" ")[0];
+
+            int expiredMonth = map.get(type);
+
+            int[] expiredYMD = new int[3];
+            expiredYMD = getExpiredYMD(aymd, expiredMonth);
+
+            if(expiredYMD[0] < year) {
+                answerList.add(index);
+            } else if (expiredYMD[0] == year) {
+                if(expiredYMD[1] < month) {
+                    answerList.add(index);
+                } else if (expiredYMD[1] == month) {
+                    if(expiredYMD[2] < day) {
+                        answerList.add(index);
+                    }
+                }
+            }
+            index++;
+        }
+
+        answer = answerList.stream().mapToInt(Integer::intValue).toArray();
+
+        return answer;
     }
+
+    public int[] getExpiredYMD(String aymd, int month) {
+        String[] ymd = aymd.split("\\.");
+        int ayear = Integer.parseInt(ymd[0]);
+        int amonth = Integer.parseInt(ymd[1]);
+        int aday = Integer.parseInt(ymd[2]);
+
+        int totalMonth = ayear * 12 + (amonth - 1) + month;
+
+        int eyear = totalMonth / 12;
+        int emonth = totalMonth % 12 + 1;
+        int eday = aday - 1;
+
+        if (eday == 0) {
+            eday = 28;
+            emonth--;
+
+            if (emonth == 0) {
+                emonth = 12;
+                eyear--;
+            }
+        }
+
+        return new int[]{eyear, emonth, eday};
+    }
+
 }
